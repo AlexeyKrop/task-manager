8a3f9e38dbaf05c2ce5bdae2f7b14422212daa92
Aleksey K <kropotin_aleksey@mail.ru>
2025-10-11 20:19:45 +0400

    fix main ts

--- src/main.ts
+++ src/main.ts
@@ -3,43 +3,46 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { apiReference } from '@scalar/nestjs-api-reference';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
-    'http://localhost:5173',
-  ];
 
-  const isDevelopment = process.env.NODE_ENV === 'development';
-  app.enableCors();
-  // if (isDevelopment) {
-  //   app.enableCors({
-  //     origin: true,
-  //     credentials: true,
-  //   });
-  //   console.log('CORS: Development mode - all origins allowed');
-  // } else {
-  //   app.enableCors({
-  //     origin: (origin, callback) => {
-  //       if (!origin) {
-  //         return callback(null, true);
-  //       }
+  const configService = app.get(ConfigService);
 
-  //       if (allowedOrigins.includes(origin)) {
-  //         callback(null, true);
-  //       } else {
-  //         console.warn(`CORS blocked: ${origin}`);
-  //         callback(new Error('Not allowed by CORS'));
-  //       }
-  //     },
-  //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  //     allowedHeaders: ['Content-Type', 'Authorization'],
-  //     credentials: true,
-  //   });
-  //   console.log(
-  //     `CORS: Production mode - allowed origins: ${allowedOrigins.join(', ')}`,
-  //   );
-  // }
+  const isDevelopment = configService.get<string>('NODE_ENV') !== 'production';
+  const allowedOrigins = configService
+    .get<string>('ALLOWED_ORIGINS')
+    ?.split(',') || ['http://localhost:5173'];
+
+  if (isDevelopment) {
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+    console.log('CORS: Development mode - all origins allowed');
+  } else {
+    app.enableCors({
+      origin: (origin, callback) => {
+        if (!origin) {
+          return callback(null, true);
+        }
+
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          console.warn(`CORS blocked: ${origin}`);
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    });
+    console.log(
+      `CORS: Production mode - allowed origins: ${allowedOrigins.join(', ')}`,
+    );
+  }
 
   const config = new DocumentBuilder()
     .setTitle('Task Manager API')
